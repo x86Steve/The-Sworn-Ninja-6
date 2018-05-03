10b41c2cab9664a94aef76822d0106d4e66d9c29
C0deFlex <30961978+C0deFlex@users.noreply.github.com>
2018-05-03 16:36:06 -0700

    Update TextUserInterface.java

--- src/edu/cpp/cs/cs141/prog_assgmnt_3/TextUserInterface.java
+++ src/edu/cpp/cs/cs141/prog_assgmnt_3/TextUserInterface.java
@@ -1,20 +1,3 @@
-/**
- * CS 141: Intro to Programming and Problem Solving
- * Professor: Edwin Rodriguez
- * <p>
- * Programming Assignment #3 (Final)
- * <p>
- * Sworn Ninja 6
- * <p>
- * Team Red
- * <p>
- * Kevin @kscroggs  - kscrogginsjr@gmail.com
- * Albert @alberthwang  - albert.is.hwang@gmail.com
- * Steve @Steve | C0deFlex  - scoseguera@cpp.edu
- * Jimmy @jimmy  - jdojimmy@gmail.com
- * Min @minimineral  -  minoook@hotmail.com
- * Dan @Dan L. - Djluoma@cpp.edu
- */
 
 package testpack;
 
