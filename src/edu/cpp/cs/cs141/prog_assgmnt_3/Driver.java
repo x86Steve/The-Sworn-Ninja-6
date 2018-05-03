d1ffe355464c495cae6d8ef9d3ee5740b6f752a8
C0deFlex <30961978+C0deFlex@users.noreply.github.com>
2018-05-03 16:31:42 -0700

    Update Driver.java

--- src/edu/cpp/cs/cs141/prog_assgmnt_3/Driver.java
+++ src/edu/cpp/cs/cs141/prog_assgmnt_3/Driver.java
@@ -1,23 +1,5 @@
 package testpack;
 
-/**
- * CS 141: Intro to Programming and Problem Solving
- * Professor: Edwin Rodriguez
- * <p>
- * Programming Assignment #3 (Final)
- * <p>
- * Sworn Ninja 6
- * <p>
- * Team Red
- * <p>
- * Kevin @kscroggs  - kscrogginsjr@gmail.com
- * Albert @alberthwang  - albert.is.hwang@gmail.com
- * Steve @Steve | C0deFlex  - scoseguera@cpp.edu
- * Jimmy @jimmy  - jdojimmy@gmail.com
- * Min @minimineral  -  minoook@hotmail.com
- * Dan @Dan L. - Djluoma@cpp.edu
- */
-
 public class Driver
 {
 	public static void main (String args[])
